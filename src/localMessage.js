b8e731fbefe4d16694ddbd8e82ac12506b8abfe2
Steven Hoglund <sjhoglund@gmail.com>
2020-04-20 11:35:40 -0700

    Initial Commit

---
+++ src/localMessage.js
@@ -0,0 +1,73 @@
+export const message = {
+  "tables": {
+    "DEFAULT": [
+      {
+        "dimension": ["33.9512, -118.3356"],
+        "metric": [1],
+      },
+      {
+        "dimension": ["33.9512, -118.3353"],
+        "metric": [30],
+      },
+      {
+        "dimension": ["33.9512, -118.3454"],
+        "metric": [15],
+      },
+      {
+        "dimension": ["33.9512, -118.3374"],
+        "metric": [150],
+      },
+      {
+        "dimension": ["33.9512, -118.3358"],
+        "metric": [200],
+      },
+      {
+        "dimension": ["33.9512, -118.3360"],
+        "metric": [45],
+      },
+      {
+        "dimension": ["33.9512, -118.3332"],
+        "metric": [26],
+      },
+    ],
+  },
+  "fields": {
+    "dimension": [
+      {
+        "id": "qt_u9c1ah4m7b",
+        "name": "Latitude,Longitude",
+        "type": "GEO",
+        "concept": "DIMENSION"
+      }
+    ],
+    "metric": [
+      {
+        "id": "qt_sp7iuh4m7b",
+        "name": "Visitors",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ],
+  },
+  "style": {
+    "fontFamily": {
+      "defaultValue": 'Roboto',
+    },
+    "districtOuter": {
+      "value": "#ddd",
+      "defaultValue": "#ddd"
+    },
+    "districtInner": {
+      "value": "#000",
+      "defaultValue": "#000"
+    },
+    "bubbleColor": {
+      "value": "#f5a442",
+      "defaultValue": "#f5a442",
+    },
+    "interpolation": {
+      "value": 'interpolateCubehelix',
+      "defaultValue": 'interpolateRgb',
+    },
+  }
+};
